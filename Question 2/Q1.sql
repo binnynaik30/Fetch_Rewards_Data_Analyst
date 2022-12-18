13a6334671e3839a2738557d61fee4cb98145866
binnynaik30 <66138253+binnynaik30@users.noreply.github.com>
2022-12-18 03:05:25 -0500

    Update Q1.sql

--- Question 2/Q1.sql
+++ Question 2/Q1.sql
@@ -2,7 +2,7 @@ SELECT top 5
 	i.brandCode,
 	SUM(ti.finalPrice) AS spend
 FROM transactionItems AS ti
-INNER JOIN (SELECT month(max(dateScanned)) - 2 m, year(MAX(dateScanned)) y FROM receipts) AS recent ON 1=1
+INNER JOIN (SELECT month(MAX(dateScanned)) - 2 m, year(MAX(dateScanned)) y FROM receipts) AS recent ON 1 = 1
 INNER JOIN transactions AS t ON ti.transactionId = t.id
 INNER JOIN receipts AS r ON t.receiptId = r.id AND month(r.dateScanned) = recent.m AND year(r.dateScanned) = recent.y
 INNER JOIN items AS i ON ti.barcode = i.barcode
