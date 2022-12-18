57ab94b15fdee83a20bedee8d24617a23e3f08ae
binnynaik30 <66138253+binnynaik30@users.noreply.github.com>
2022-12-18 02:50:34 -0500

    Create Q6.sql

---
+++ Question 2/Q6.sql
@@ -0,0 +1,6 @@
+SELECT User.user_id, Users.createdDate, Receipts.receipt_id as rid, Receipts.totalSpent, Transaction.receipt_id as tid, Transaction.brand_id as tbid, brand._id
+FROM ((( Users JOIN Receipts ON User.user_id = Receipts.userid)
+         JOIN Transactions on tid = Receipts.receipt_id)
+         JOIN Brands on Brand.brand_id = tbid)
+         COUNT (rewardsReceiptItemList)
+         WHERE createdDate = Dateadd(Month, datediff(Month, 0, Dateadd(m, -6, current_timestamp)), 0)
